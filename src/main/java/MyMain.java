3cd6aebe7d272ec24bea8abb249a1209d1a48f1c
23kmccarrick <23kmccarrick@athenian.org>
2021-01-21 01:01:35 +0000

    oh

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,9 +1,14 @@
+
 public class MyMain {
     // This method returns true/false if there 
     // was a boat the specified coordinates. This
     // method also prints out an appropriate message
     public static boolean hit(boolean[][] board, int row, int col) { 
-        // YOUR CODE HERE
+        if(board[row][col]==true){
+            System.out.println("There was a hit!");
+            return true;
+        }
+        System.out.println("You missed a boat!");
         return false;
     }
 
@@ -13,7 +18,14 @@ public class MyMain {
     // The remaining pieces are placed in the direction given
     // by the direction input
     public static boolean[][] placeBoat(boolean[][] board, String direction, int boatLength, int row, int col) { 
-        // YOUR CODE HERE
+        board[row][col]= true;
+        for(int i = 0; i <boatLength; i++){
+            if(direction=="down"){
+                board[row+i][col]=true;
+            }else{
+                board[row][col+i]=true;
+            }
+        }
         return board;
     }
 
@@ -22,8 +34,16 @@ public class MyMain {
     // increasing length
     // You may assume that all Strings are lowercase 
     public static boolean inOrder(String[][] words) { 
-        // YOUR CODE HERE
-        return false;
+        for(int i = 0; i < words.length; i++){
+            for(int j = 1; j < words[0].length; j++){
+                if(words[i][j].length()<=words[i][j-1].length()){
+                    return false;
+                }else if(words[i][j-1].length() == words[i][j].length() && words[i][j-1].compareTo(words[i][j-1]) < 0){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
